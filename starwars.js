a7ad5bb81734a128f9e1a3bc045dd4f05ecd73c6
RebeckaOscarsson <70890347+RebeckaOscarsson@users.noreply.github.com>
2020-11-02 23:21:40 +0100

    Klart!

--- starwars.js
+++ starwars.js
@@ -3,7 +3,6 @@
 
 async function getData(url) {
 
-
   let response = await fetch(url);
   if (!response.ok) {
     throw new Error(response.status)
@@ -19,18 +18,120 @@ async function getData(url) {
 //Kör funktionen som hämtar och skriver ut data
 
 let pageNumber = 1;
+const firstPage = 'http://swapi.dev/api/people/?page=1'
 const footer = document.querySelector(".page");
+let numberofPages;
 
 
-let firstPage = getData('http://swapi.dev/api/people/?page=1')
+getData(firstPage)
   .then(function (response) {
     printNames(response); //skriver ut namn på 10 karaktärer
-    let numberofPages = Math.ceil(response.count / 10); //räknar hur många sidor och rundar uppåt
+    numberofPages = Math.ceil(response.count / 10); //räknar hur många sidor och rundar uppåt
     footer.append(`${pageNumber} / ${numberofPages}`); //skriver ut sidnummer
   })
   .catch(function (error) {
-    alert("det gick inte att ladda sidan, felkod" + error)
+    alert("det gick inte att ladda sidan, " + error)
   })
+
+
+//Skriver ut namnen på karaktärer och lägger till eventListeners
+function printNames(data) {
+
+  const nameList = document.querySelector(".characters");
+  erase(nameList)
+
+  for (i = 0; i < data.results.length; i++) {
+    let person = data.results[i]
+    let personalProperties = ["Name", "Height", "Mass", "Hair color", "Skin color", "Eye color", "Birth year", "Gender"]
+    let personalData = [person.name, person.height, person.mass, person.hair_color, person.skin_color, person.eye_color, person.birth_year, person.gender]
+
+    const newA = document.createElement("a");
+    newA.setAttribute("href", "#");
+    newA.append(person.name);
+
+    newA.addEventListener("click", function () {
+      printPersonalData(personalProperties, personalData)
+    })
+    newA.addEventListener("click", function () {
+      getPlanet(person.homeworld)
+    })
+
+    newA.addEventListener("click", function () {
+      createTabs(person.homeworld, person.species, person.vehicles, person.starships)
+    })
+    console.log(person.species)
+    nameList.appendChild(newA);
+
+  }
+}
+
+
+//Gå bakåt och framåt
+const backLink = document.querySelector(".back");
+backLink.addEventListener("click", goBack);
+
+const forwardLink = document.querySelector(".forward");
+forwardLink.addEventListener("click", goForward);
+const nameList = document.querySelector(".characters");
+
+function goBack() {
+
+  if (pageNumber > 1) {
+    erase(nameList)
+
+    let loading = document.querySelector(".loading");
+    loading.classList.remove('hidden');
+
+    pageNumber--;
+
+    let url = `http://swapi.dev/api/people/?page=${pageNumber}`;
+
+    getData(url)
+      .then(function (response) {
+
+        erase(footer);
+        footer.append(`${pageNumber} / ${numberofPages}`)
+
+        printNames(response);
+        loading.classList.add('hidden');
+      })
+      .catch(function (error) {
+        alert("det gick inte att ladda sidan, " + error)
+      })
+  }
+}
+
+
+
+function goForward() {
+
+  if (pageNumber < numberofPages) {
+    erase(nameList);
+
+    let loading = document.querySelector(".loading");
+    loading.classList.remove('hidden');
+
+    pageNumber++;
+
+    let url = `http://swapi.dev/api/people/?page=${pageNumber}`;
+
+    getData(url)
+      .then(function (response) {
+
+        if (response.next != "null") {
+
+          erase(footer);
+          footer.append(`${pageNumber} / ${numberofPages}`)
+
+          printNames(response);
+          loading.classList.add('hidden');
+        }
+      })
+      .catch(function (error) {
+        alert("det gick inte att ladda sidan, " + error)
+      })
+  }
+}
 
 
 //sudda
@@ -43,13 +144,13 @@ function erase(element) {
 }
 
 //Skriver ut listorna till höger
-function printDetails(properties, values, list){
+function printDetails(properties, values, list) {
 
 
   for (i = 0; i < properties.length; i++) {
 
     if (i == 0) {
-      newH3 = document.createElement("h3");
+      let newH3 = document.createElement("h3");
       newH3.append(values[i]);
       list.appendChild(newH3);
     } else {
@@ -58,6 +159,8 @@ function printDetails(properties, values, list){
       list.appendChild(newLi);
     }
   }
+  let br = document.createElement("br");
+  list.appendChild(br);
 }
 
 //Skriver ut info om karaktärerna när man klickar på dem
@@ -65,22 +168,22 @@ function printPersonalData(properties, values) {
 
   let detailsList = document.querySelector(".details");
   erase(detailsList)
-  
+
   let loading = document.querySelector(".loadingTopRight");
   loading.classList.remove('hidden');
 
-  printDetails(properties, values, detailsList)
+  printDetails(properties, values, detailsList);
 
   loading.classList.add('hidden');
 }
 
 
-//Skriver ut info om hemplanet när man klickar på figurerna
+//Skriver ut info om hemplanet när man klickar på karaktärerna
 function getPlanet(planet) {
 
   const planetList = document.querySelector(".planet");
-  erase(planetList)
-  
+  erase(planetList);
+
   let loading = document.querySelector(".loadingBottomRight");
   loading.classList.remove('hidden');
 
@@ -96,101 +199,103 @@ function getPlanet(planet) {
 }
 
 
-//Skriver ut namnen på karaktärer och lägger till eventListeners
-function printNames(data) {
+//skriver ut infon för flikarna
+function getTabData(tabData) {
 
-  const nameList = document.querySelector(".characters");
-  erase(nameList)
+  const planetList = document.querySelector(".planet");
+  erase(planetList);
 
-  for (i = 0; i < data.results.length; i++) {
-    let person = data.results[i]
-    let personalProperties = ["Name", "Height", "Mass", "Hair color", "Skin color", "Eye color", "Birth year", "Gender"]
-    let personalData = [person.name, person.height, person.mass, person.hair_color, person.skin_color, person.eye_color, person.birth_year, person.gender]
-    
-    const newA = document.createElement("a");
-    newA.setAttribute("href", "#");
-    newA.append(person.name);
-
-    newA.addEventListener("click", function () {
-      printPersonalData(personalProperties, personalData)
-    })
-    newA.addEventListener("click", function () {
-      getPlanet(person.homeworld)
-    })
-
-    nameList.appendChild(newA);
-
-  }
-
-}
-
-
-
-//Gå bakåt och framåt
-
-
-let backLink = document.querySelector(".back");
-backLink.addEventListener("click", goBack);
-
-let forwardLink = document.querySelector(".forward");
-forwardLink.addEventListener("click", goForward);
-
-
-function goBack() {
-
-  let nameList = document.querySelector(".characters");
-
-
-  if (pageNumber > 1) {
-    erase(nameList)
-
-
-    let loading = document.querySelector(".loading");
-    loading.classList.remove('hidden');
-    pageNumber--;
-    let url = `http://swapi.dev/api/people/?page=${pageNumber}`;
-
-    getData(url)
-      .then(function (response) {
-
-        let numberofPages = Math.ceil(response.count / 10); //räknar hur många sidor och rundar uppåt
-
-        erase(footer);
-        footer.append(`${pageNumber} / ${numberofPages}`)
-
-        printNames(response);
-        loading.classList.add('hidden');
-      })
-  }
-}
-
-
-
-function goForward() {
-  let nameList = document.querySelector(".characters");
-  erase(nameList);
-
-  let loading = document.querySelector(".loading");
+  let loading = document.querySelector(".loadingBottomRight");
   loading.classList.remove('hidden');
 
-  let url = `http://swapi.dev/api/people/?page=${pageNumber}`;
-  getData(url)
-    .then(function (response) {
+  if (tabData.length == 0) {
+    const newH3 = document.createElement("h3");
+    loading.classList.add('hidden');
+    newH3.append("No data available");
+    planetList.appendChild(newH3);
+  } else {
 
-      let numberofPages = Math.ceil(response.count / 10); //räknar hur många sidor och rundar uppåt
+    tabData.forEach(function (item, index) {
+      getData(tabData[index])
 
-      if (response.next != "null") {
-        pageNumber++;
+        .then(function (response) {
 
-        erase(footer);
-        footer.append(`${pageNumber} / ${numberofPages}`)
+          let speciesProperties = Object.keys(response);
+          let speciesValues = Object.values(response);
 
-        printNames(response);
-        loading.classList.add('hidden');
-      }
+          printDetails(speciesProperties, speciesValues, planetList);
+
+          loading.classList.add('hidden');
+        })
+        .catch(function (error) {
+          alert(error)
+        })
+
     })
+  }
 }
 
+//gör att aktiv flik har annan färg
+function activateTab(tab) {
+  var allTabs = document.querySelectorAll(".tabs>a");
+
+  for (var i = 0; i < allTabs.length; i++) {
+    allTabs[i].className = "inactive";
+  }
+  tab.className = "active";
+
+}
+
+function createTabs(planet, species, vehicles, starships) {
+  const tabs = document.querySelector(".tabs")
+  erase(tabs);
+
+  let newA0 = document.createElement("a");
+  newA0.setAttribute("href", "#");
+  newA0.className = "active";
+  newA0.append("Planet");
+  newA0.addEventListener("click", function () {
+    getPlanet(planet)
+  })
+  newA0.addEventListener("click", function () {
+    activateTab(newA0)
+  })
+  tabs.appendChild(newA0);
+
+  let newA = document.createElement("a");
+  newA.setAttribute("href", "#");
+  newA.append("Species");
+  newA.addEventListener("click", function () {
+    getTabData(species)
+  })
+  newA.addEventListener("click", function () {
+    activateTab(newA)
+  })
+
+  tabs.appendChild(newA);
+
+  let newA2 = document.createElement("a");
+  newA2.setAttribute("href", "#");
+  newA2.append("Vehicles");
+  newA2.addEventListener("click", function () {
+    getTabData(vehicles)
+  })
+  newA2.addEventListener("click", function () {
+    activateTab(newA2)
+  })
+  tabs.appendChild(newA2);
+
+  let newA3 = document.createElement("a");
+  newA3.setAttribute("href", "#");
+  newA3.append("Starships");
+  newA3.addEventListener("click", function () {
+    getTabData(starships)
+  })
+  newA3.addEventListener("click", function () {
+    activateTab(newA3)
+  })
+  tabs.appendChild(newA3);
+}
 
 
 //visar loading första gången sidan hämtas
